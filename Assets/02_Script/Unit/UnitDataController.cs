50c94e09b2c0ed466f30fa56e60d008cf3ff6349
dkdkdsa <bos94330@gmail.com>
2023-11-04 23:18:47 +0900

    UnitDataController

--- Assets/02_Script/Unit/UnitDataController.cs
+++ Assets/02_Script/Unit/UnitDataController.cs
@@ -5,9 +5,33 @@ using UnityEngine;
 public class UnitDataController : MonoBehaviour
 {
 
+    [Header("°ª")]
     [SerializeField] private float attackPower;
     [SerializeField] private float defenceValue;
     [SerializeField] private float attackTime;
     [SerializeField] private float skillCoolDown;
+    [SerializeField] private LayerMask layer;
+    [Header("AI")]
+    [SerializeField] private float attackRange;
+    [SerializeField] private float skillRange;
+
+#if UNITY_EDITOR
+
+    private void OnDrawGizmos()
+    {
+        
+        var old = Gizmos.color;
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, skillRange);
+
+        Gizmos.color = old;
+
+    }
+
+#endif
 
 }
