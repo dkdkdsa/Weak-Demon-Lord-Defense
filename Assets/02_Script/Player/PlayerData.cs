c6a27e465ac28a2a7b21a9b787b07b1b97bb6428
dkdkdsa <bos94330@gmail.com>
2023-11-05 16:53:28 +0900

    PlayerData

--- Assets/02_Script/Player/PlayerData.cs
+++ Assets/02_Script/Player/PlayerData.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public int Money
     {
-        
+
+        get
+        {
+
+            return money;
+
+        }
+
+        set
+        {
+
+            money = value;
+            OnValueChanged?.Invoke();
+
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private int money;
+
+    public event Action OnValueChanged;
+
 }
